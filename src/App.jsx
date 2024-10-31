412616aa445c519e96d8bcf200f385b5335c1594
CongBinh01 <thaicongbinh27301@gmail.com>
2024-10-31 16:16:28 +0700

    Config router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import HomePage from '@components/HomePage/HomePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Blog from './components/Blog/Blog';
+import routers from '@/routers/routers';
+import { Suspense } from 'react';
 
 function App() {
   return ( 
-    <>
-        <HomePage />
-    </>
+    //Higher Component boc tat ca Component vao ben trong
+   <BrowserRouter>
+      <Suspense fallback={ <div>Loading...</div>}>
+        <Routes>
+        {
+          routers.map((item, index) => {
+              return <Route key={index} 
+              path={item.path} 
+              //Khi component dang duoc goi trong trang thai loading
+              //Them de xu ly UI loading
+              element={<item.component /> } />
+          })
+        }
+        </Routes>
+      </Suspense>
+
+
+   </BrowserRouter>
   );
 }
 
